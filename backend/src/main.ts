613d1e2505873f7024f4c2ba1d3fa722cb3fd131
rawrr0x <chevdarevmac28@gmail.com>
2026-04-05 13:30:31 +0300

    backend init

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'WebChat',
+    }),
+  });
+
+  const PORT = process.env.PORT || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('WebChat')
+    .setDescription('WebChat API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT, () => Logger.log(`Server started on ${PORT}`));
 }
+
 bootstrap();
